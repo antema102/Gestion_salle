7b6eddb26b47f4311f04326ba0fc04baf343f886
antema102 <antema103@gmail.com>
2023-06-05 12:34:03 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import {createRoot} from "react-dom/client"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import 'react-big-scheduler/lib/css/style.css';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-big-scheduler/lib/css/style.css";
+import 'react-calendar/dist/Calendar.css'
 import App from './App';
 
 
